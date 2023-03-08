ba9fd353142105dc5d376b0ec067c5f00e14a49e
Andreea B <ANDREEA@DESKTOP-2OIJ242>
2023-03-08 10:02:02 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace PrimeNumbers2nd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SubmitBtn = new System.Windows.Forms.Button();
+            this.Number = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // SubmitBtn
+            // 
+            this.SubmitBtn.BackColor = System.Drawing.Color.RosyBrown;
+            this.SubmitBtn.Font = new System.Drawing.Font("Kievit Offc Pro", 23F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.SubmitBtn.Location = new System.Drawing.Point(281, 212);
+            this.SubmitBtn.Name = "SubmitBtn";
+            this.SubmitBtn.Size = new System.Drawing.Size(199, 105);
+            this.SubmitBtn.TabIndex = 0;
+            this.SubmitBtn.Text = "Submit";
+            this.SubmitBtn.UseVisualStyleBackColor = false;
+            this.SubmitBtn.Click += new System.EventHandler(this.SubmitBtn_Click);
+            // 
+            // Number
+            // 
+            this.Number.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Number.Location = new System.Drawing.Point(308, 100);
+            this.Number.Name = "Number";
+            this.Number.Size = new System.Drawing.Size(149, 26);
+            this.Number.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Number);
+            this.Controls.Add(this.SubmitBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SubmitBtn;
+        private System.Windows.Forms.TextBox Number;
+    }
+}
+
